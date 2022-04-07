f04d890fa5fbc02f60e16dc69886548d864d8ebb
Denys Demydenko <kawoonne@gmail.com>
2022-04-07 21:37:33 +0300

    finish

--- App.js
+++ App.js
@@ -1,38 +1,14 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const {writeFile} = require('querystring');
-const {Buffer} = require('querystring');
 const formidable = require('formidable');
 
-
 http.createServer((req, res) => {
-
     if (req.url === '/') {
         console.log('main page');
         sendRes('index.html', 'text/html', res);
     } else if (/\/uploads\/[^\/]+$/.test(req.url) && req.method === 'POST') {
-        sendRes2(req.url, getContentFile(req.url), res, req);
-        //saveFile(req);
-        //sendRes(req.url,getContentFile(req.url), res);
-
-        // res.writeHead(200, {'Content-Type': 'image/jpeg'});
-        // let data = '';
-        // req.on('data', function(chunk) {
-        //     data += chunk;
-        // });
-        // req.on('end', function() {
-        //     console.log(data);
-        // });
-        // res.end('done');
-        // fs.writeFile(__dirname+'/static/images/imgg2.jpg', data, function(err) {
-        //     if(err) {
-        //         return console.log(err);
-        //     }
-        //     console.log("The file was saved!");
-        // });
-
-
+        fileSave(req.url, getContentFile(req.url), res, req);
     } else {
         sendRes(req.url, getContentFile(req.url), res);
     }
@@ -58,91 +34,25 @@ function sendRes(url, contentType, res) {
     })
 }
 
-
-function sendRes2(url, contentType, res, req) {
-
+function fileSave(url, contentType, res, req) {
     let form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-        console.log(files['file']['filepath']);
-        //console.log(fs.statSync(files));
-        let oldpath = files['file']['filepath'];
-        console.log();
-        let ar = url.split('/');
-        console.log(ar);
-        let newpath = path.join(__dirname + '/static/images/', ar[2]);
-        console.log(newpath);
-        fs.rename(oldpath, newpath, function (err) {
-            if (err) throw err;
-            res.write('File uploaded and moved!');
+        if(err){
+            console.log('не вдалося розпарсить прийом запиту');
             res.end();
-        });
+        }
+        else{
+            let oldpath = files['file']['filepath'];
+            let ar = url.split('/');
+            let newpath = path.join(__dirname + '/static/images/', ar[2]);
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+        }
     });
-
-    // let file = path.join(__dirname, url);
-    // let pathF = `${file}`;
-    // let fileReq = req;
-    // //console.log('MyPath: '+pathF);
-    // //console.log(file.content);
-    // //console.log(fileReq);
-    //
-    // let t = req.pipe(
-    //     fs.createWriteStream('req')
-    // ).on('finish', () => res.end('ok'));
-    // console.log(t.);
-
-
-
-
-
-
-    // let form = new formidable.IncomingForm();
-    //
-    // //Process the file upload in Node
-    // form.parse(req, function (error, fields, file) {
-    //     let filepath = file.filepath;
-    //     let newpath = pathF;
-    //     //newpath += file.fileupload.originalFilename;
-    //
-    //     //Copy the uploaded file to a custom folder
-    //     console.log(newpath);
-    //     console.log(filepath);
-    //     fs.rename(filepath, newpath, function () {
-    //         //Send a NodeJS file upload confirmation message
-    //         res.write('NodeJS File Upload Success!');
-    //         res.end();
-    //     });
-    // });
-
-
-
-
-
-
-    // fs.readFile(file, 'utf-8', (err, data) => {
-    //     if(err){
-    //         console.log(err);
-    //
-    //     }
-    //     else{
-    //         console.log(data);
-    //     }
-    //
-    // });
-
-
-    // fs.open(pathF, 'w', (err) => {
-    //     if (err) throw err;
-    //     console.log('File created');
-    // });
-
-
-
-
-    res.end();
-
-
 }
-
 
 function getContentFile(url) {
     switch (path.extname(url)) {
@@ -158,30 +68,5 @@ function getContentFile(url) {
             return 'application/ico';
         default:
             return 'application/octet-stream';
-            break;
     }
-}
-
-function saveFile(data, resp) {
-    console.log(data);
-
-    resp.sendFile(data);
-
-    // let body = '';
-    // data.on('data', chunk=>{
-    //     body += chunk.toString()
-    // });
-    // let file = data
-    // console.log(file.name);
-
-
-    fs.writeFile(__dirname + '/static/images', data, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    });
-
-    resp.sendFile(data);
-
 }
--- static/js/upload.js
+++ static/js/upload.js
@@ -1,23 +1,14 @@
 function uploadFile(event){
-    console.log('upload');
     let target = event.target || event.currentTarget;
     let file = target.files[0];
-
-    console.log('file: ' + file.name);
     let xhr = new XMLHttpRequest();
-    console.log(1);
     xhr.open('POST', '/uploads/'+file.name, true);
-    console.log(2);
     xhr.setRequestHeader('Content-Type', 'application/octet-stream');
-    console.log(3);
-    console.log(xhr.onreadystatechange);
     xhr.onreadystatechange = function (){
-        console.log('funczapros');
         event = null;
         if(xhr.readyState === 4){
             if (xhr.status === 200){
                 console.log('ok');
-                func(this.responseText);
             }
             else{
                 console.log('error');
@@ -26,10 +17,6 @@ function uploadFile(event){
     }
     xhr.send(file);
     event.target.value = '';
-}
-
-function func(data){
-    console.log(data);
 }
 
 document.querySelector('#f').addEventListener('change', uploadFile);
